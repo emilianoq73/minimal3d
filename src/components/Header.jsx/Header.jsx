8700d2ed1e5f60eb9ced7a5da8ce2c80527f2304
Sergio Quipildor <emilianoq73@gmail.com>
2022-08-06 23:40:59 -0300

    Creando Header

---
+++ src/components/Header.jsx/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar.jsx/Navbar'
+
+const Header = () => {
+  return (
+    <header className="App-header">
+        <Navbar></Navbar>
+
+    </header>
+  )
+}
+
+export default Header
